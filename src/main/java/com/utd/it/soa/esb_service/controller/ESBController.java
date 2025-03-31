3ff15d98dc15a9d6295682851caa128ba330bcb0
Diego Daher Diaz Contreras <169078085+DiegoDaher@users.noreply.github.com>
2025-03-31 00:43:10 -0600

    Update ESBController.java

--- src/main/java/com/utd/it/soa/esb_service/controller/ESBController.java
+++ src/main/java/com/utd/it/soa/esb_service/controller/ESBController.java
@@ -39,7 +39,7 @@ public class ESBController {
 
         // Enviar la petición correctamente con Content-Type JSON
         String response = webClient.post()
-                .uri("http://user-service:3000/api/users")
+                .uri("/")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .bodyValue(user)
                 .retrieve()
@@ -60,7 +60,7 @@ public class ESBController {
 
         // Realizar la petición GET con el token
         String response = webClient.get()
-                .uri("http://user-service:3000/api/users/all")
+                .uri("/all")
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .retrieve()
                 .bodyToMono(String.class)
@@ -83,7 +83,7 @@ public class ESBController {
 
         // Realizar la petición PUT con el token
         String response = webClient.put()
-                .uri("http://user-service:3000/api/users/" + id)
+                .uri("/" + id)
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .bodyValue(user)
                 .retrieve()
@@ -106,7 +106,7 @@ public class ESBController {
 
         // Realizar la petición PATCH con el token
         String response = webClient.patch()
-                .uri("http://user-service:3000/api/users/" + id)
+                .uri("/" + id) // localhost:4000
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .retrieve()
                 .bodyToMono(String.class)
